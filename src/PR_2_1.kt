407ab498822315cb9ffb49f72cd6a75d98569c78
borisov <hoppano@mail.ru>
2020-09-14 22:09:31 +0300

    refactoring

--- src/PR_2_1.kt
+++ src/PR_2_1.kt
@@ -4,20 +4,22 @@
 1. как обычную функцию;
 2. как tailrec-функцию.*/
 
-fun common (input: String): Char {
-    var max = '0'
-    for (num: Char in input) if (num > max && num.toInt() % 2 == 0) max = num
-    return  max
-}
 
-tailrec fun recursive(input: String, max: Char, n: Int = 0): Char = when {
-    n == input.length - 1 -> max
-    input.length == 1 -> input[0]
-    (input[n] > max && input[n].toInt() % 2 == 0) -> recursive(input, input[n], n+1 )
-    else -> recursive(input, max, n+1 )
-}
 
 fun main () {
+    fun common (input: String): Char {
+        var max = '0'
+        for (num: Char in input) if (num > max && num.toInt() % 2 == 0) max = num
+        return  max
+    }
+
+    tailrec fun recursive(input: String, max: Char, n: Int = 0): Char = when {
+        n == input.length - 1 -> max
+        input.length == 1 -> input[0]
+        (input[n] > max && input[n].toInt() % 2 == 0) -> recursive(input, input[n], n+1 )
+        else -> recursive(input, max, n+1 )
+    }
+
     println("Введите неотрицательное число и я найду максимальную чётную цифру: ")
     val input = readLine()
     if (input!!.toIntOrNull() != null) {
